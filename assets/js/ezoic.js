9412781185b4bb3df99342dc3c698071616bdf0e
obstrendteam <observabilitrends@gmail.com>
2025-09-29 20:00:25 +0200

    Create ezoic.js

---
+++ assets/js/ezoic.js
@@ -0,0 +1,12 @@
+<!-- assets/js/ezoic.js -->
+
+<!-- PRIVACY SCRIPTS (GDPR/CCPA compliance) -->
+<script src="https://cmp.gatekeeperconsent.com/min.js" data-cfasync="false"></script>
+<script src="https://the.gatekeeperconsent.com/cmp.min.js" data-cfasync="false"></script>
+
+<!-- EZOIC HEADER SCRIPT -->
+<script async src="//www.ezojs.com/ezoic/sa.min.js"></script>
+<script>
+    window.ezstandalone = window.ezstandalone || {};
+    ezstandalone.cmd = ezstandalone.cmd || [];
+</script>
