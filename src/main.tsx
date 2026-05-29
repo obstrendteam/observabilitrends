ec41aa50eca84018f179e334b2beac587242bb2f
obstrendteam <observabilitrends@gmail.com>
2026-05-29 17:48:57 +0200

    Replace App component with header element

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <h1 style={{ color: "white" }}>
+    TEST OBSERVABILITRENDS
+  </h1>
+);
