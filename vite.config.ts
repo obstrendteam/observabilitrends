a761a5546385d30318541b240d1d28a4e2b4f96c
obstrendteam <observabilitrends@gmail.com>
2026-05-29 12:30:34 +0200

    Change base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/observabilitrends/",   // ← OBLIGATORIO PARA GH PAGES
+  base: "/",
   server: {
     host: "::",
     port: 8080,
