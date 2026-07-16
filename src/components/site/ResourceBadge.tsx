8fe57047e610fc0fc29503b7a35070bf2bb42767
obstrendteam <observabilitrends@gmail.com>
2026-07-16 17:15:55 +0200

    Add ResourceBadge component with children prop

--- src/components/site/ResourceBadge.tsx
+++ src/components/site/ResourceBadge.tsx
@@ -1,1 +1,12 @@
 
+interface Props {
+  children: React.ReactNode;
+}
+
+export function ResourceBadge({ children }: Props) {
+  return (
+    <span className="mono text-xs px-2 py-1 rounded bg-primary/10 text-primary border border-primary/20">
+      {children}
+    </span>
+  );
+}
